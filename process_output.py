0388e33736cb5b2b12dd8b2807fe0c748e79ff84
Andrew Garcia <garcia.gtr@gmail.com>
2019-10-03 15:32:14 -0400

    fixed bulk tab delimited file plot processing; fwf needs to have high infer_nrows

--- process_output.py
+++ process_output.py
@@ -40,20 +40,85 @@ def lastRow(idx, workbook, col=1):
     return lwr_cell.row
 
 
+'''MAKE AN XY PLOT FOR A SINGLE EXCEL FILE (SPECIFY FOLDER PATH AND FILE NAME)'''
 def readxl(path,file):
 
     book = xw.Book(path+file)
     
     x=book.sheets['Sheet1'].range('A1:A'+str(lastRow('Sheet1',book))).value
     y=book.sheets['Sheet1'].range('B1:B'+str(lastRow('Sheet1',book))).value
-    
+    book.close()
+    plt.figure()
     plt.plot(x,y)
     plt.title(file[:-5])
-    plt.xlabel('time  /  ps')
+    plt.xlabel('time')
     plt.savefig(file+'.png')
-      
+    
+    
+'''MAKE AN XY PLOT WITH TWO INDEPENDENT VARS (Y AND Z) FOR A SINGLE EXCEL FILE
+  (SPECIFY FOLDER PATH AND FILE NAME)'''
+def readxl2(path,file):
 
-'''SPECIFIC EXCEL FILE '''
-path = r'C:\Users\***\file_folder/'
-file = '*.xlsx'
-readxl(path,file)
+    book = xw.Book(path+file)
+    
+    x=book.sheets['Sheet1'].range('A1:A'+str(lastRow('Sheet1',book))).value
+    y=book.sheets['Sheet1'].range('B1:B'+str(lastRow('Sheet1',book))).value
+    z=book.sheets['Sheet1'].range('C1:C'+str(lastRow('Sheet1',book))).value
+
+    book.close()
+    plt.figure()
+    plt.plot(x,y,label = r'y')
+    plt.plot(x,z,label = r'z')
+
+    plt.title(file[:-5])
+    plt.xlabel('time')
+    plt.savefig(file+'.png')
+    plt.legend()
+
+
+
+'MAKE XY PLOTS FOR ALL TAB DELIMITED FILES IN A FOLDER (SPECIFY FOLDER PATH)'
+def writetab_bulk(path):
+    asps = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if not file.endswith('.xlsx') and not file.endswith('.csv')\
+            and not file.endswith('.png') and not file.endswith('.txt')\
+            and not file.endswith('.xls'):
+                asps.append(file)
+    print(asps)
+    
+    index=1
+    for file in asps:
+        df= pandas.read_fwf(path+file,header=None,infer_nrows=10000)
+        xdim, ydim = df.shape[0], df.shape[1]
+        
+        if xdim > 1 and ydim == 2:
+            df.columns =['a','b'] 
+            
+            print(file,index)
+#            print(df)
+            plt.figure()
+            plt.plot(df['a'],df['b'])
+            plt.title(file)
+            plt.xlabel('time  /  ps')
+            plt.savefig(file+'.png')
+#            plt.close()
+            index+=1
+#            return df
+  
+'(optional): create function with directory path (keep uncommented if unaware)'
+from proc_out_key import pathkey
+path = pathkey()  
+
+
+'''COMMAND SECTION (INPUT)'''
+
+'MAKE XY PLOTS FOR ALL TAB DELIMITED FILES IN A FOLDER (SPECIFY FOLDER PATH)'
+#path = r'C:\Users\***\file_folder/'
+df=writetab_bulk(path)
+
+'''MAKE AN XY PLOT FOR A SINGLE EXCEL FILE (SPECIFY FOLDER PATH AND FILE NAME)'''
+#path = r'C:\Users\***\file_folder/'
+#file = '**.xlsx'
+#writexl(path,file)

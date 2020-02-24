26658af02a8b91952d7d6606094e7c870e3cb2c5
Andrew Garcia <garcia.gtr@gmail.com>
2020-02-24 18:30:35 -0500

    added extract_y.py

--- extract_y.py
+++ extract_y.py
@@ -13,22 +13,22 @@ import matplotlib.pyplot as plt
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--path", 
-                default = r'C:\Users\garci\...\folder/',
-                type = str, help="path to csv files")
+                default = r'C:\Users\garci\...\folder/', type = str, 
+                help="path to csv files")
 ap.add_argument("-r", "--reference_column", 
-                default = "Wavelength",
-                type = str, help="name of reference column for indexing")
+                default = "Wavelength", type = str, 
+                help="name of reference column for indexing")
 ap.add_argument("-i", "--indexed_column", 
-                default = "Abs",
-                type = str, help="name of column which is indexed based on reference column")
+                default = "Abs", type = str, 
+                help="name of column which is indexed based on reference column")
 ap.add_argument("-c", "--string_cutoff", 
-                default = "M",
-                type = str, help="if file name contains the x-value up to a certain string character\
+                default = "M",type = str,
+                help="if file name contains the x-value up to a certain string character\
                 this cutoff will define where the x-value (float) will end (e.g. file = 5001.45Go.csv with \
                 -c G will give 5001.45")
 ap.add_argument("-l", "--lookup_value", 
-                default = 555,
-                type = float, help="lookup value for reference column")
+                default = None, type = float,
+                help="lookup value for reference column")
 args = vars(ap.parse_args())
 
 

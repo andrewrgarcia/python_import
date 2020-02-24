1cf32bd2bac53cf639e9a31b096618602b32ee55
Andrew Garcia <garcia.gtr@gmail.com>
2020-02-24 18:21:43 -0500

    add extract_y

--- extract_y.py
+++ extract_y.py
@@ -9,25 +9,54 @@ Created on Mon Feb 24 11:49:38 2020
 import os
 import pandas 
 import argparse
+import matplotlib.pyplot as plt
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--path", 
-                default = r'C:\Users\..',
+                default = r'C:\Users\garci\DROPBO~1\Research\_Shared-Undergraduate-Files\Copper_Sulfate_Cycling_Growth\data\2.12\CalibrationMeasurements/',
                 type = str, help="path to csv files")
+ap.add_argument("-r", "--reference_column", 
+                default = "Wavelength",
+                type = str, help="name of reference column for indexing")
+ap.add_argument("-i", "--indexed_column", 
+                default = "Abs",
+                type = str, help="name of column which is indexed based on reference column")
+ap.add_argument("-c", "--string_cutoff", 
+                default = "M",
+                type = str, help="if file name contains the x-value up to a certain string character\
+                this cutoff will define where the x-value (float) will end (e.g. file = 5001.45Go.csv with \
+                -c G will give 5001.45")
+ap.add_argument("-l", "--lookup_value", 
+                default = 555,
+                type = float, help="lookup value for reference column")
 args = vars(ap.parse_args())
 
 
 def readcsv(path,file):
-    df = pandas.read_csv(path + file,header=20)  
-    print(df)
+    df = pandas.read_csv(path + file,header=15)  
+    df = df.set_index(args["reference_column"])
+    y = df[args["indexed_column"]].loc[args["lookup_value"]]
+#    print(df)
+    return y
 #    
 def make(path):
-    
+   
+    x=[]
+    y= []
     asps = []
     for root, dirs, files in os.walk(path):
         for file in files:
             asps.append(file)
-            readcsv(path,file)
+            if args["string_cutoff"] is not "***":
+                x.append(float(file.split(args["string_cutoff"])[0])) 
+            else:
+                x.append(float(file)) 
+            y.append(readcsv(path,file))
+#            print(readcsv(path,file))
+            
+    plt.plot(x,y)
+    print('x : {} \n y: {}'.\
+         format(x,y))
 
-#    print(asps)
 
 make(args["path"])
